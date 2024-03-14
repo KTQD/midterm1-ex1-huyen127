ca88592d0c0e5711f70b06907b50e58bac4d3b8a
huyen127 <129976351+huyen127@users.noreply.github.com>
2024-03-14 07:56:23 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        MP3 mp3;
+        MP3 mp3 = new MP3("MP3", "Portable music player", "MP123", 50.00f, "3 hours");
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Java", "OOP with Java", "OOP1", 25.1f, "John Smith", 320, "learning");
         book.showInfo();
-
     }
 }
